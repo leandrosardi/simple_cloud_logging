429be296b03efae16681793faf8ec1b8bf67bf4c
DEV1\Leandro <leandro.sardi@expandedventure.com>
2021-03-29 16:24:49 -0300

    leandrosardi/blackstack#207

--- lib/remotelogger.rb
+++ lib/remotelogger.rb
@@ -16,16 +16,19 @@ module BlackStack
     # call the save method to store the new attributes into the data file
     def reset()
       super
+=begin
       url = "#{self.api_protocol}://#{self.api_domain}:#{self.api_port.to_s}/api1.4/scl/reset.json"
       res = BlackStack::Netting::api_call(url, {
         'api_key' => self.api_key,
         'filename' => self.filename,
         'id_client' => self.id_client,
       })
+=end
     end
 
     def log(s, datetime=nil)
       ltext = super(s, datetime)
+=begin
       url = "#{self.api_protocol}://#{self.api_domain}:#{self.api_port.to_s}/api1.4/scl/log.json"
       res = BlackStack::Netting::api_call(url, {
         'api_key' => self.api_key,
@@ -34,11 +37,13 @@ module BlackStack
         'method' => BlackStack::BaseLogger::METHOD_LOG, 
         'id_client' => self.id_client,
       })
+=end
       ltext
     end
 
     def logs(s, datetime=nil)
       ltext = super(s, datetime)
+=begin
       url = "#{self.api_protocol}://#{self.api_domain}:#{self.api_port.to_s}/api1.4/scl/log.json"
       res = BlackStack::Netting::api_call(url, {
         'api_key' => self.api_key,
@@ -47,11 +52,13 @@ module BlackStack
         'method' => BlackStack::BaseLogger::METHOD_LOGS, 
         'id_client' => self.id_client,
       })
+=end
       ltext
     end
 
     def logf(s, datetime=nil)
       ltext = super(s, datetime)
+=begin
       url = "#{self.api_protocol}://#{self.api_domain}:#{self.api_port.to_s}/api1.4/scl/log.json"
       res = BlackStack::Netting::api_call(url, {
         'api_key' => self.api_key,
@@ -60,16 +67,19 @@ module BlackStack
         'method' => BlackStack::BaseLogger::METHOD_LOGF, 
         'id_client' => self.id_client,
       })
+=end
       ltext
     end    
 
     def release()
+=begin
       url = "#{self.api_url}:#{self.api_port.to_s}/api1.4/scl/release.json"
       res = BlackStack::Netting::api_call(url, {
         'api_key' => self.api_key,
         'filename' => self.filename, 
         'id_client' => self.id_client,
       })
+=end
     end
   end
 end # module BlackStack
